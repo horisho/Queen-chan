808c03c64508bba23cac627a4b5ffc686fb5f3f8
horisho <oh.yes.i.know@gmail.com>
2024-07-28 01:51:07 +0900

    pgn2gifを実装

--- cogs/chess_cog.py
+++ cogs/chess_cog.py
@@ -1,8 +1,9 @@
 import discord
 from discord.ext import commands
-import chess, chess.svg
+import chess, chess.svg, chess.pgn
 import cairosvg
 import io
+from PIL import Image
 
 class ChessCog(commands.Cog, name="Chess commands"):
     def __init__(self, bot):
@@ -12,7 +13,6 @@ class ChessCog(commands.Cog, name="Chess commands"):
     async def on_ready(self):
         print('Chess Cog on ready!')
 
-    # /fen2img
     @commands.command(
         help="Convert FEN to PNG format image.",
         usage=(
@@ -37,8 +37,6 @@ class ChessCog(commands.Cog, name="Chess commands"):
             image_binary.seek(0)
             await ctx.send(file=discord.File(fp=image_binary, filename='fen.png'))
 
-
-    # /fen2img_with_details
     @commands.command(
         help="Convert FEN and Advanced Settings to PNG format image.",
         usage=(
@@ -78,6 +76,63 @@ class ChessCog(commands.Cog, name="Chess commands"):
         with io.BytesIO(png_data) as image_binary:
             image_binary.seek(0)
             await ctx.send(file=discord.File(fp=image_binary, filename='fen.png'))
+
+    @commands.command(
+        help="Convert PGN to GIF format.",
+        usage=(
+            "/pgn2gif <PGN>"
+        )
+    )
+    async def pgn2gif(self, ctx, *, pgn_str: str = None):
+        # メッセージの添付ファイルが存在する場合
+        if ctx.message.attachments:
+            file = ctx.message.attachments[0]
+            if file.filename.endswith(".pgn"):
+                pgn_str = await file.read()
+                pgn_str = pgn_str.decode('utf-8')
+            else:
+                await ctx.send("Please upload a PGN file.")
+                return
+        elif pgn_str:
+            # コマンド引数としてPGN文字列が提供された場合
+            pgn_str = pgn_str
+        else:
+            await ctx.send("You must provide either a PGN string or upload a PGN file.")
+            return
+
+        pgn_io = io.StringIO(pgn_str)
+        game = chess.pgn.read_game(pgn_io)
+        board = game.board()
+        pov = chess.WHITE
+        if game.headers.get("Result", "*") == "0-1":
+            pov = chess.BLACK
+        images = []
+        prev_board = board.copy()
+        for move in game.mainline_moves():
+            board.push(move)
+            # 差分を計算
+            arrows = [ (move.from_square, move.to_square) ]
+            svg_data = chess.svg.board(
+                board,
+                orientation=pov,
+                arrows=arrows,
+                size=400
+            )
+            png_data = cairosvg.svg2png(bytestring=svg_data.encode('utf-8'))
+            image = Image.open(io.BytesIO(png_data))
+            images.append(image)
+
+        with io.BytesIO() as gif_io:
+            images[0].save(
+                gif_io,
+                format='GIF',
+                save_all=True,
+                append_images=images[1:],
+                duration=500,
+                loop=0
+            )
+            gif_io.seek(0)
+            await ctx.send(file=discord.File(fp=gif_io, filename='pgn.gif'))
 
 async def setup(bot):
     await bot.add_cog(ChessCog(bot))
